16bf44d84571474e943dc084af9aeb613f8038c5
ooishikoon <80800379+ooishikoon@users.noreply.github.com>
2022-12-19 20:25:18 +0800

    Add files via upload

---
+++ JavaDoc_Assignment_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"my.uum","l":"App"},{"p":"my.uum","l":"myBot"},{"p":"my.uum","l":"SQLite"}];updateSearchResults();
